3dd67bff67afae38c618c4321b7d153b1a2e6858
ajikha19 <145316669+ajikha19@users.noreply.github.com>
2023-11-12 21:18:08 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiInputDataMahasiswa
+{
+    public partial class Form1 : Form
+    {
+        private List<Mahasiswa> list = new List<Mahasiswa>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InisialisasiListView();
+        }
+
+        private void InisialisasiListView()
+        {
+            lvwMahasiswa.View = View.Details;
+            lvwMahasiswa.FullRowSelect = true;
+            lvwMahasiswa.GridLines = true;
+
+            lvwMahasiswa.Columns.Add("No.", 30, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nim.", 91, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nama.", 200, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Kelas.", 70, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nilai.", 50, HorizontalAlignment.Center);
+        }
+           
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwMahasiswa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+
+            mhs.Nim = txtNim.Text;
+            mhs.Nama = txtNama.Text;
+            mhs.Kelas = txtKelas.Text;
+            mhs.Nilai = int.Parse(txtNilai.Text);
+
+            list.Add(mhs);
+
+            var msg = "Data mahasiswa berhasil disimpan.";
+
+            MessageBox.Show(msg, "Informasi", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            //ResetForm();
+        }
+
+        private void TampilkanData()
+        {
+            lvwMahasiswa.Items.Clear();
+
+            foreach (var mhs in list)
+            {
+                var noUrut = lvwMahasiswa.Items.Count + 1;
+
+                var item = new ListViewItem(noUrut.ToString());
+                item.SubItems.Add(mhs.Nim);
+                item.SubItems.Add(mhs.Nama);
+                item.SubItems.Add(mhs.Kelas);
+                item.SubItems.Add(mhs.Nilai.ToString());
+
+                lvwMahasiswa.Items.Add(item);
+            }
+        }
+        private void btnTampilkanData_Click(object sender, EventArgs e)
+        {
+            TampilkanData();
+        }
+    }
+}
